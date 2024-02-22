180c2330265fc455abfaac38f49d5910892019ab
Bridget Kessler <106347277+bridget-kessler@users.noreply.github.com>
2024-02-22 10:37:29 -0600

    Add new categories

--- src/utils/useRandomArt.constants.ts
+++ src/utils/useRandomArt.constants.ts
@@ -1,5 +1,5 @@
 import { IArt } from "../types/types";
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
 
 export const categories: {
   [key: string]: string[] | Array<{ title: string; id: string }>;
@@ -19,18 +19,29 @@ export const categories: {
     "Juan Sanchez Cotan",
     "Edouard Manet",
     "Odilon Redon",
-    "James McNeil Whistler"
+    "James McNeil Whistler",
   ],
   style: [
     {
       title: "19th Century",
       id: "TM-13203",
     },
-    { title: "Japanese", id: "TM-13203" },
+    { title: "Arts and Crafts Movement", id: "TM-7569" },
+    { title: "Folk Art", id: "TM-12125" },
+    { title: "Pictorialism", id: "TM-7546" },
+    { title: "Korean", id: "TM-5182" },
+    { title: "Syrian", id: "TM-4531" },
+    { title: "Peruvian", id: "TM-10469" },
+    { title: "Greek", id: "TM-4408" },
     { title: "Chinese", id: "TM-4303" },
+    { title: "Eqyptian", id: "TM-4805" },
+    { title: "Persian", id: "TM-4811" },
   ],
   subject: [
     { title: "Cityscapes", id: "TM-8762" },
+    { title: "Architecture", id: "TM-12085" },
+    { title: "Green (Color)", id: "TM-11849" },
+    { title: "Plants", id: "TM-12058" },
     { title: "Landscapes", id: "TM-8657" },
   ],
 };
@@ -59,4 +70,3 @@ export const getRandomCategory: () => {
     title: randomCategory,
   };
 };
-
